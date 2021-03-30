97331001bf8d90b3a0aed8e3e33f3e9b13b3b988
rcantin <ryan.cantin@coxautoinc.com>
2021-03-30 18:11:25 -0400

    Changed homepage back to path and added basename to router

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Contact from "./components/Contact";
 
 export default function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div>
         <Banner />
         <div className="container">
